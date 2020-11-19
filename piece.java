d5729f1cf7bf2b266f0e80d5031c329bd4393049
lavaramen <72640395+lavaramen@users.noreply.github.com>
2020-11-18 22:04:25 -0600

    Add files via upload

---
+++ piece.java
@@ -0,0 +1,18 @@
+public class piece {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public piece() {
+    }
+
+    public piece(String name) {
+        this.name = name;
+    }
+}
