2ee1a3b3fab38088f40b6040406f17723517657a
TKuhner <71107536+TKuhner@users.noreply.github.com>
2020-11-21 11:02:50 -0600

    Add files via upload
    Board class with comments. Ready for other teams to receive.

--- Board.java
+++ Board.java
@@ -3,33 +3,48 @@ package com.company;
 import java.util.ArrayList;
 
 public class Board {
+    //2D array to make the 8 by 8 grid of the board
     Square [][] squares = new Square [8][8];
 
+    //arraylist to store the black pieces
     ArrayList<Piece> blackPiece = new ArrayList<>();
+    //arraylist to store the white pieces
     ArrayList<Piece> whitePiece = new ArrayList<>();
 
+    //board constructor
     public Board(){
         this.setBoard();
     }
 
 
+    //sets the board at the beginning of the game
     public void setBoard(){
         // need 8x8 grid of squares
+        //for loop builds columns
         for(int x = 0; x <8; x++){
+            //nested for loop builds rows
             for(int y = 0; y < 8; y++){
+                //2d array of squares using 8x8 grid makes board
+                //using each square made by the for loop and nested for loop to iterate through
+                //all 64 squares of the board
                 squares[x][y] = new Square();
             }
         }
 
+        //for loop to cycle through rows to establish setup of board
         for(int x = 0; x< 8; x++){
+            //switch to establish each row
             switch(x){
                 case 0:
+                    //setting back row for black side
                     setBack(x, blackPiece,"B");
                     break;
                 case 1:
+                    //setting front row for black side
                     setFront(x, blackPiece, "B");
                     break;
                 case 2:
+                    //setting blank rows ↓
                     setBlanks(x);
                     break;
                 case 3:
@@ -39,12 +54,15 @@ public class Board {
                     setBlanks(x);
                     break;
                 case 5:
+                    //setting blank rows ↑
                     setBlanks(x);
                     break;
                 case 6:
+                    //setting front row for white side
                     setFront(x, whitePiece,"W");
                     break;
                 case 7:
+                    //setting back row for white side
                     setBack(x, whitePiece, "W");
                     break;
             }
@@ -54,26 +72,42 @@ public class Board {
     }
 
 
+    //sets the front row of pieces
     public void setFront(int row, ArrayList<Piece> pieces, String color){
         for(int y = 1; y < 2; y++){
+            //make piece object
             Piece thisPiece = new Piece();
+            //calls function and passes piece object with coordinates
             setPieceOnSquare(thisPiece,row,y);
+            //passes color and coordinates
             thisPiece.setName("--" + color + row + "-" + y + "-");
+            //keeps track of pieces in arraylist
             pieces.add(thisPiece);
         }
     }
 
 
 
+    //sets the back row pieces
+    //as the order of the back row is the same for both the black and white
+    //the function just has to cycle through the same order for both sides of the board
     public void setBack(int row, ArrayList<Piece> pieces, String color){
+        //the order of the back row is rook, knight, bishop, queen, king, bishop, knight, rook, from left to right
+        //so we used a switch to build each specific piece as the for loop iterates through each square
         for(int y = 1; y < 2; y++){
             switch(y){
                 case 0:
+                    // creates piece object
                     Piece rookPiece = new Piece();
+                    // calls function to set piece on the square while passing the object and coordinate
                     setPieceOnSquare(rookPiece,row,y);
+                    //sets the falue of the piece
                     rookPiece.setName("--" + color + row + "-" + y + "-");
+                    //keeps track of pieces in arraylist
                     pieces.add(rookPiece);
                     break;
+
+                    //other cases do the same as this case, but for their respective pieces based on where they should be on the board
                 case 1:
                     Piece knightPiece = new Piece();
                     setPieceOnSquare(knightPiece,row,y);
@@ -122,36 +156,52 @@ public class Board {
     }
 
 
+    //sets blank rows at the beginning of the game
     public void setBlanks(int row){
         for(int y = 0; y < 8; y++) {
+            //makes empty piece object
             Piece emptyPiece = new Piece();
+            //calls set function to pass empty piece and coordinates
             setPieceOnSquare(emptyPiece, row, y);
+            //sets the "blank" value of the empty piece
             emptyPiece.setName("__" + row + "-" + y + "__");
         }
     }
 
 
+    //sets piece on square
     public void setPieceOnSquare(Piece piece, int x, int y) {
+        //gets passed the piece and coordinates
+        //then sets the the piece at the specific coordinate
         squares[x][y].setPiece(piece);
     }
 
 
+    //remove pieces from board
     public void removePieceOnSpace(int x, int y){
+        //gets passed coordinates to tell what square to remove piece from
+        //makes new piece object
         Piece emptyPiece = new Piece();
-        emptyPiece.setName("__" + x + "-" +y+ "__");
+        //sets the object as blank instead of as a piece
+        emptyPiece.setName("__" + x + "-" + y + "__");
+        //sets the blank object to the specific square
         squares[x][y].setPiece(emptyPiece);
     }
 
+    //Display the board
     public void showBoard(){
         for(int x=0; x<8; x++){
             for(int y = 0; y<8; y++){
+                //displays the square in each column
                 System.out.println(squares[x][y].getPiece());
             }
+            //displays the square in each row
+            //starts displaying a  new row of squares once
+            // all previous squares were displayed in the previous row
             System.out.println();
         }
     }
 
 
 }
-
 
