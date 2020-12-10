115538b6790453837d2ca9beb8615d410a69602d
MuteMounds <66083716+MuteMounds@users.noreply.github.com>
2020-12-09 21:50:45 -0600

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,8 +1,10 @@
-package com.company;
-
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Board {
+public class Board implements Serializable {
     //2D array to make the 8 by 8 grid of the board
     Square [][] squares = new Square [8][8];
 
@@ -11,38 +13,57 @@ public class Board {
     //arraylist to store the white pieces
     ArrayList<Piece> whitePiece = new ArrayList<>();
 
+
+
     //board constructor
     public Board(){
         this.setBoard();
     }
 
+    public Square[][] getSquares() {
+        return squares;
+    }
+
+    public void setSquares(Square[][] squares) {
+        this.squares = squares;
+    }
+
+    public ArrayList<Piece> getBlackPiece() {
+        return blackPiece;
+    }
+
+    public void setBlackPiece(ArrayList<Piece> blackPiece) {
+        this.blackPiece = blackPiece;
+    }
+
+    public ArrayList<Piece> getWhitePiece() {
+        return whitePiece;
+    }
+
+    public void setWhitePiece(ArrayList<Piece> whitePiece) {
+        this.whitePiece = whitePiece;
+    }
 
     //sets the board at the beginning of the game
     public void setBoard(){
+        char xcord = 0;
+        char ycord = 0;
+
         // need 8x8 grid of squares
         //for loop builds columns
         for(int x = 0; x <8; x++){
             //nested for loop builds rows
             for(int y = 0; y < 8; y++){
+
+
+
+
                 //2d array of squares using 8x8 grid makes board
                 //using each square made by the for loop and nested for loop to iterate through
                 //all 64 squares of the board
                 squares[x][y] = new Square();
             }
         }
-      /*  public void resetBoard(){
-
-            Scanner kb = new Scanner(System.in);
-            String reset = "";
-            reset.equalsIgnoreCase("reset");
-            try {
-                if (kb.hasNext(reset)) {
-                    setBoard();
-                }
-            } catch (Exception exception) {
-                System.out.println("Not a valid input");
-            }
-        }*/
 
         //for loop to cycle through rows to establish setup of board
         for(int x = 0; x< 8; x++){
@@ -50,11 +71,11 @@ public class Board {
             switch(x){
                 case 0:
                     //setting back row for black side
-                    setBack(x, blackPiece,"B");
+                    setBack(x, blackPiece,"B", false);
                     break;
                 case 1:
                     //setting front row for black side
-                    setFront(x, blackPiece, "B");
+                    setFront(x, blackPiece, "B", false);
                     break;
                 case 2:
                     //setting blank rows ↓
@@ -72,11 +93,11 @@ public class Board {
                     break;
                 case 6:
                     //setting front row for white side
-                    setFront(x, whitePiece,"W");
+                    setFront(x, whitePiece,"W", true);
                     break;
                 case 7:
                     //setting back row for white side
-                    setBack(x, whitePiece, "W");
+                    setBack(x, whitePiece, "W", true);
                     break;
             }
         }
@@ -86,16 +107,91 @@ public class Board {
 
 
     //sets the front row of pieces
-    public void setFront(int row, ArrayList<Piece> pieces, String color){
-        for(int y = 1; y < 2; y++){
+    public void setFront(int row, ArrayList<Piece> pieces, String color, Boolean isWhite){
+
+        char xcord = 0;
+        char ycord = 0;
+
+        if (row == 0)
+        {
+            xcord = 'A';
+        }
+        if (row == 1)
+        {
+            xcord = 'B';
+        }
+        if (row == 2)
+        {
+            xcord = 'C';
+        }
+        if (row == 3)
+        {
+            xcord = 'D';
+        }
+        if (row == 4)
+        {
+            xcord = 'E';
+        }
+        if (row == 5)
+        {
+            xcord = 'F';
+        }
+        if (row == 6)
+        {
+            xcord = 'G';
+        }
+        if (row == 7)
+        {
+            xcord = 'H';
+        }
+
+
+        for(int y = 0; y < 8; y++){
+
+
+            if (y == 0)
+            {
+                ycord = '8';
+            }
+            if (y == 1)
+            {
+                ycord = '7';
+            }
+            if (y == 2)
+            {
+                ycord = '6';
+            }
+            if (y == 3)
+            {
+                ycord = '5';
+            }
+            if (y == 4)
+            {
+                ycord = '4';
+            }
+            if (y == 5)
+            {
+                ycord = '3';
+            }
+            if (y == 6)
+            {
+                ycord = '2';
+            }
+            if (y == 7)
+            {
+                ycord = '1';
+            }
+
+
+
             //make piece object
-            Piece thisPiece = new Piece();
+            Pawn pawnPiece = new Pawn();
             //calls function and passes piece object with coordinates
-            setPieceOnSquare(thisPiece,row,y);
+            setPieceOnSquare(pawnPiece,row,y);
             //passes color and coordinates
-            thisPiece.setName("--" + color + row + "-" + y + "-");
+            //thisPiece.setName("--" + color + "pawn-" +  xcord + "-" + ycord + "-");
             //keeps track of pieces in arraylist
-            pieces.add(thisPiece);
+            pieces.add(pawnPiece);
         }
     }
 
@@ -104,63 +200,136 @@ public class Board {
     //sets the back row pieces
     //as the order of the back row is the same for both the black and white
     //the function just has to cycle through the same order for both sides of the board
-    public void setBack(int row, ArrayList<Piece> pieces, String color){
+    public void setBack(int row, ArrayList<Piece> pieces, String color, Boolean isWhite){
+
+
+
+        char xcord = 0;
+        char ycord = 0;
+
+        if (row == 0)
+        {
+            xcord = 'A';
+        }
+        if (row == 1)
+        {
+            xcord = 'B';
+        }
+        if (row == 2)
+        {
+            xcord = 'C';
+        }
+        if (row == 3)
+        {
+            xcord = 'D';
+        }
+        if (row == 4)
+        {
+            xcord = 'E';
+        }
+        if (row == 5)
+        {
+            xcord = 'F';
+        }
+        if (row == 6)
+        {
+            xcord = 'G';
+        }
+        if (row == 7)
+        {
+            xcord = 'H';
+        }
+
+
+
         //the order of the back row is rook, knight, bishop, queen, king, bishop, knight, rook, from left to right
         //so we used a switch to build each specific piece as the for loop iterates through each square
-        for(int y = 1; y < 2; y++){
+        for(int y = 0; y < 8; y++){
+
+            if (y == 0)
+            {
+                ycord = '8';
+            }
+            if (y == 1)
+            {
+                ycord = '7';
+            }
+            if (y == 2)
+            {
+                ycord = '6';
+            }
+            if (y == 3)
+            {
+                ycord = '5';
+            }
+            if (y == 4)
+            {
+                ycord = '4';
+            }
+            if (y == 5)
+            {
+                ycord = '3';
+            }
+            if (y == 6)
+            {
+                ycord = '2';
+            }
+            if (y == 7)
+            {
+                ycord = '1';
+            }
+
             switch(y){
                 case 0:
                     // creates piece object
-                    Piece rookPiece = new Piece();
+                    Rook rookPiece = new Rook(row, y, "--" + color + "-rk1-" + xcord + "-" + ycord + "-", isWhite);
                     // calls function to set piece on the square while passing the object and coordinate
                     setPieceOnSquare(rookPiece,row,y);
-                    //sets the falue of the piece
-                    rookPiece.setName("--" + color + row + "-" + y + "-");
                     //keeps track of pieces in arraylist
                     pieces.add(rookPiece);
                     break;
 
-                    //other cases do the same as this case, but for their respective pieces based on where they should be on the board
+                //other cases do the same as this case, but for their respective pieces based on where they should be on the board
                 case 1:
-                    Piece knightPiece = new Piece();
+                    Knight knightPiece = new Knight(row, y, "--" + color + "-kn1-" + xcord + "-" + ycord + "-", isWhite);
                     setPieceOnSquare(knightPiece,row,y);
-                    knightPiece.setName("--" + color + row + "-" + y + "-");
+                    //knightPiece.setName("--" + color + "-kn1-" + xcord + "-" + ycord + "-");
                     pieces.add(knightPiece);
                     break;
                 case 2:
-                    Piece bishopPiece = new Piece();
+                    Bishop bishopPiece = new Bishop(row, y, "--" + color + "-bi1-" + xcord + "-" + ycord + "-", isWhite);
                     setPieceOnSquare(bishopPiece,row,y);
-                    bishopPiece.setName("--" + color + row + "-" + y + "-");
+                    //bishopPiece.setName("--" + color + "-bi1-" + xcord + "-" + ycord + "-");
                     pieces.add(bishopPiece);
                     break;
                 case 3:
-                    Piece queenPiece = new Piece();
+                    Queen queenPiece = new Queen(row, y, "--" + color + "-qu1-" + xcord + "-" + ycord + "-", isWhite);
                     setPieceOnSquare(queenPiece,row,y);
-                    queenPiece.setName("--" + color + row + "-" + y + "-");
+                    //queenPiece.setName("--" + color + "-qu1-" + xcord + "-" + ycord + "-");
                     pieces.add(queenPiece);
                     break;
                 case 4:
-                    Piece kingPiece = new Piece();
+                    King kingPiece = new King(row, y, "--" + color + "-kg1-" + xcord + "-" + ycord + "-" , isWhite);
                     setPieceOnSquare(kingPiece,row,y);
-                    kingPiece.setName("--" + color + row + "-" + y + "-");
+                    //kingPiece.setName("--" + color + "-kg1-" + xcord + "-" + ycord + "-");
                     pieces.add(kingPiece);
                     break;
                 case 5:
-                    Piece bishopPiece1 = new Piece();
+                    Bishop bishopPiece1 = new Bishop(row, y, "--" + color + "-bi2-" + xcord + "-" + ycord + "-", isWhite);
                     setPieceOnSquare(bishopPiece1,row,y);
-                    bishopPiece1.setName("--" + color + row + "-" + y + "-");
+                    //bishopPiece1.setName("--" + color + "-bi2-" + xcord + "-" + ycord + "-");
                     pieces.add(bishopPiece1);
                     break;
                 case 6:
-                    Piece knightPiece1 = new Piece();
+                    Knight knightPiece1 = new Knight(row, y, "--" + color + "-kn2-" + xcord + "-" + ycord + "-", isWhite);
                     setPieceOnSquare(knightPiece1,row,y);
-                    knightPiece1.setName("--" + color + row + "-" + y + "-");
+                    //knightPiece1.setName("--" + color + "-kn2-" + xcord + "-" + ycord + "-");
                     pieces.add(knightPiece1);
                     break;
                 case 7:
-                    Piece rookPiece1 = new Piece();
+                    Rook rookPiece1 = new Rook(row, y, "--" + color + "-rk2-" + xcord + "-" + ycord + "-", isWhite);
                     setPieceOnSquare(rookPiece1,row,y);
-                    rookPiece1.setName("--" + color + row + "-" + y + "-");
+                    //rookPiece1.setName("--" + color + "-rk2-" + xcord + "-" + ycord + "-");
                     pieces.add(rookPiece1);
                     break;
             }
@@ -171,14 +340,101 @@ public class Board {
 
     //sets blank rows at the beginning of the game
     public void setBlanks(int row){
+
+        char xcord = 0;
+        char ycord = 0;
+
+        if (row == 0)
+        {
+            xcord = 'A';
+        }
+        if (row == 1)
+        {
+            xcord = 'B';
+        }
+        if (row == 2)
+        {
+            xcord = 'C';
+        }
+        if (row == 3)
+        {
+            xcord = 'D';
+        }
+        if (row == 4)
+        {
+            xcord = 'E';
+        }
+        if (row == 5)
+        {
+            xcord = 'F';
+        }
+        if (row == 6)
+        {
+            xcord = 'G';
+        }
+        if (row == 7)
+        {
+            xcord = 'H';
+        }
+
         for(int y = 0; y < 8; y++) {
+
+
+            if (y == 0)
+            {
+                ycord = '8';
+            }
+            if (y == 1)
+            {
+                ycord = '7';
+            }
+            if (y == 2)
+            {
+                ycord = '6';
+            }
+            if (y == 3)
+            {
+                ycord = '5';
+            }
+            if (y == 4)
+            {
+                ycord = '4';
+            }
+            if (y == 5)
+            {
+                ycord = '3';
+            }
+            if (y == 6)
+            {
+                ycord = '2';
+            }
+            if (y == 7)
+            {
+                ycord = '1';
+            }
+
+
             //makes empty piece object
             Piece emptyPiece = new Piece();
             //calls set function to pass empty piece and coordinates
             setPieceOnSquare(emptyPiece, row, y);
             //sets the "blank" value of the empty piece
-            emptyPiece.setName("__" + row + "-" + y + "__");
+            //emptyPiece.setName("____" + xcord + "--" + ycord + "____");
         }
+    }
+
+    public int receiveMove(Piece piece, String input){
+        int x = 0;
+        int y = 0;
+
+
+
+
+
+
+
+
+        return x + y;
     }
 
 
@@ -192,28 +448,164 @@ public class Board {
 
     //remove pieces from board
     public void removePieceOnSpace(int x, int y){
-        //gets passed coordinates to tell what square to remove piece from
-        //makes new piece object
-        Piece emptyPiece = new Piece();
-        //sets the object as blank instead of as a piece
-        emptyPiece.setName("__" + x + "-" + y + "__");
-        //sets the blank object to the specific square
-        squares[x][y].setPiece(emptyPiece);
+
+
+        char xcord = 0;
+        char ycord = 0;
+
+        if (x == 0)
+        {
+            xcord = 'A';
+        }
+        if (x == 1)
+        {
+            xcord = 'B';
+        }
+        if (x == 2)
+        {
+            xcord = 'C';
+        }
+        if (x == 3)
+        {
+            xcord = 'D';
+        }
+        if (x == 4)
+        {
+            xcord = 'E';
+        }
+        if (x == 5)
+        {
+            xcord = 'F';
+        }
+        if (x == 6)
+        {
+            xcord = 'G';
+        }
+        if (x == 7)
+        {
+            xcord = 'H';
+        }
+
+        if (y == 0)
+        {
+            ycord = '1';
+        }
+        if (y == 1)
+        {
+            ycord = '2';
+        }
+        if (y == 2)
+        {
+            ycord = '3';
+        }
+        if (y == 3)
+        {
+            ycord = '4';
+        }
+        if (y == 4)
+        {
+            ycord = '5';
+        }
+        if (y == 5)
+        {
+            ycord = '6';
+        }
+        if (y == 6)
+        {
+            ycord = '7';
+        }
+        if (y == 7)
+        {
+            ycord = '8';
+        }
+
+        try {
+            //gets passed coordinates to tell what square to remove piece from
+            //makes new piece object
+            Piece emptyPiece = new Piece();
+            //sets the object as blank instead of as a piece
+            emptyPiece.setName("__" + xcord + "-" + ycord + "__");
+            //sets the blank object to the specific square
+            squares[x][y].setPiece(emptyPiece);
+        } catch(ArrayIndexOutOfBoundsException e){
+
+        }
     }
 
     //Display the board
-    public void showBoard(){
+    public ArrayList<Piece> showBoard(){
+
+
+        ArrayList<Piece> tempList = new ArrayList<>();
         for(int x=0; x<8; x++){
             for(int y = 0; y<8; y++){
                 //displays the square in each column
-                System.out.println(squares[x][y].getPiece());
+                //System.out.print(squares[x][y].getPiece().getName());
+
+
+
+                System.out.print(squares[x][y].getPiece().getName());
+                if (Piece.isAlive == true) {
+                    tempList.add(squares[x][y].getPiece());
+                }
             }
             //displays the square in each row
             //starts displaying a  new row of squares once
             // all previous squares were displayed in the previous row
             System.out.println();
         }
+        return tempList;
     }
+
+
+    public void resetBoard(){
+        for(int x = 0; x <8; x++){
+            //nested for loop to cycle through all rows and columns of board
+            for(int y = 0; y < 8; y++){
+                //remove all pieces on the board
+                removePieceOnSpace(x, y);
+            }
+        }
+
+        //set board back to beginning
+        setBoard();
+    }
+
+
+    public void saveBoard(){
+        ArrayList<Piece> tempList = new ArrayList<>();
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                tempList.add(squares[x][y].getPiece());
+            }
+        }
+        try {
+            XMLEncoder enc = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("XMLFile.xml")));
+
+            enc.writeObject(tempList);
+
+            enc.close();
+        } catch (Exception e) {
+            System.out.println("Error occurred writing to file");
+        }
+
+    }
+
+    public void loadBoard(){
+        //Board boardReader = new Board();
+        try {
+            XMLDecoder dec = new XMLDecoder(new BufferedInputStream(new FileInputStream("XMLFile.xml")));
+
+            Board boardReader = (Board) dec.readObject();
+            System.out.println();
+            dec.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+
 
 
 }
